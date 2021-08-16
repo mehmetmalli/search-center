78a9989a3a4c57d9f7abafb13ca99dfeafbe2dde
Mehmet MALLI <mallimehmet@gmail.com>
2021-08-16 18:08:53 +0300

    initial commit

---
+++ providers.js
@@ -0,0 +1,13 @@
+const providers = [{
+        name: "Tureng",
+        url: "https://tureng.com/en/turkish-english/"
+    },
+    {
+        name: "Google",
+        url: "https://www.google.com/search?q="
+    }, 
+    {
+        name: "YouTube",
+        url: "https://www.youtube.com/results?search_query="
+    }
+];
